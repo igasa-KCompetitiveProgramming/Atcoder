2dca89e75925eb48a7fcd7e6e66569e7a4002e73
r04i02 <k10190202@gmail.com>
2024-01-23 23:32:32 +0900

    支払う金額が最小のものを出力

---
+++ ABC310/a.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
+bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
+//bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
+int main(){
+    int n,p,q;
+    int d[100];
+    cin >> n >> p >> q;
+    int help=p;
+    for(int i=0;i<n;i++){
+        cin >> d[i];
+    }
+    for(int j=0;j<n;j++){
+        if(d[j]+q<p){
+            if(help>d[j]+q){
+                help=d[j]+q;
+            }
+        }
+    }
+    if(help>p){
+        cout << p;
+    }else{
+        cout << help;
+    }
+}
