3b08669bff2fed698d8945090aee11efdbe59a7d
r04i02 <k10190202@gmail.com>
2024-02-10 23:29:17 +0900

    n*log(n)(小数点は切り捨て)+2*n%(2^log(n)(小数点は切り捨て))を出力

---
+++ ABC340/c.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
+bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
+//bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
+int main(){
+    ll n;
+    cin >> n;
+    ll nc = n;
+    ll check = 0;
+    while(true){
+        if(n/2==0){
+            break;
+        }else{
+            n = n/2;
+            check++;
+        }
+    }
+    ll gens = 1;
+    rep(i,check){
+        gens *= 2;
+    }
+    cout << nc*(check) + (nc-gens)*2;
+}
