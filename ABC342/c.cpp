dc114be34664dd202c33481be554ecd683eae8a5
r04i02 <k10190202@gmail.com>
2024-02-24 22:42:14 +0900

    mapでそれぞれのアルファベットが何に変換されるか先に前計算してから文字列を変換して出力

---
+++ ABC342/c.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
+bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
+//bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
+int main(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int q;
+    cin >> q;
+    vector<char> c(q),d(q);
+    rep(i,q){
+        cin >> c[i] >> d[i];
+    }
+    char mp[26];
+    rep(i,26){
+        mp[i] = 'a'+i;
+    }
+    rep(i,q){
+        rep(j,26){
+            if(mp[j]==c[i]){
+                mp[j] = d[i];
+            }
+        }
+    }
+    rep(i,n){
+        cout << mp[s[i]-'a'];
+    }
+}
