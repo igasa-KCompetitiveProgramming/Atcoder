7e748253ebc5cc777d35fb6f8cb8fd5354902251
r04i02 <k10190202@gmail.com>
2024-04-14 21:07:48 +0900

    mapで何種類か保存してその種類をmapで保管

---
+++ ABC349/b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
+bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
+//bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
+int main(){
+    string s;
+    cin >> s;
+    map<char,int> mp;
+    map<int,int> mp2;
+    rep(i,s.size()){
+        mp[s[i]]++;
+    }
+    for(auto x:mp){
+        mp2[x.second]++;
+    }
+    for(auto x:mp2){
+        if(x.second != 0 && x.second != 2){
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+}
