b74c4ca662f99bf12fa88d61a56a01388cf0c07d
r04i02 <k10190202@gmail.com>
2024-04-30 19:33:11 +0900

    whileで一番右とひとつ左が一緒なら左1足して右を消す。できなくなったら右に足すの繰り返し

---
+++ ABC351/c.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
+bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
+//bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
+int main(){
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    vector<ll> check;
+    rep(i,n){
+        check.push_back(a[i]);
+        while(true){
+            if(check.size() == 1) break;
+            if(check[check.size()-1] == check[check.size()-2]){
+                check[check.size()-2] ++;
+                check.pop_back();
+            }else{
+                break;
+            }
+        }
+    }
+    cout << check.size() << endl;
+}
