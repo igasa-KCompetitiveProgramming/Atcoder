af3994509b03b46e2c23cebfb25845d4f1b110b0
r04i02 <k10190202@gmail.com>
2024-05-04 19:31:25 +0900

    erase()はO(N)

--- ABC350/d.cpp
+++ ABC350/d.cpp
@@ -15,57 +15,45 @@ ll INF = 9223372036854775807; //おおよそ9*10^18
 bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
 bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
 //bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
-int bfs(ll cnt, ll n, vector<vector<ll>>& friends, vector<vector<ll>>& ans, vector<bool>& seen,vector<ll>& check){
-    ll x = check[0];
-    check.erase(check.begin());
-    seen[x] = true;
-    if(ans[cnt][ans[cnt].size()-1]!=x) ans.push_back({x});
-    rep(i,friends[x].size()){
-        if(seen[friends[x][i]]){
-            continue;
-        }else{
-            ans[cnt].push_back(friends[x][i]);
-            seen[friends[x][i]] = true;
-            check.push_back(friends[x][i]);
-        }
-    }
-    if(check.empty()){
-        return 0;
-    }else{
-        bfs(cnt,n,friends,ans,seen,check);
-        return 0;
-    }
-}
-
 int main(){
     ll n,m;
     cin >> n >> m;
     vector<ll> a(m),b(m);
-    vector<vector<ll>> friends(n);
+    vector<vector<ll>> friendList(n);
     rep(i,m){
-        cin >> a[i];
-        cin >> b[i];
-        friends[a[i]-1].push_back(b[i]-1);
+        cin >> a[i] >> b[i];
+        friendList[a[i]-1].push_back(b[i]-1);
+        friendList[b[i]-1].push_back(a[i]-1);
     }
-    vector<vector<ll>> ans(n+m);
     vector<bool> seen(n,false);
-    vector<ll> check;
-    ll cnt = 0;
-    check.push_back(0);
+    vector<ll> bondNum;
     rep(i,n){
-        if(seen[i]){
-            continue;
-        }else{
-            check.push_back(i);
-            ans[cnt].push_back(i);
-            bfs(cnt,n,friends,ans,seen,check);
-            cnt++;
+        if(seen[i]) continue;
+        vector<ll> check;
+        seen[i] = true;
+        ll cnt = 0;
+        cnt++;
+        check.push_back(i);
+        while(true){
+            if(check.size()==0){
+                bondNum.push_back(cnt);
+                break;
+            }
+            seen[i] = true;
+            ll now = check[check.size()-1];
+            check.pop_back();
+            for(auto x: friendList[now]){
+                if(seen[x]) continue;
+                seen[x] = true;
+                check.push_back(x);
+                cnt++;
+            }
         }
     }
-    ll ans1 = 0;
-    rep(i,cnt){
-        ans1 += ans[i].size()*(ans[i].size()-1)/2; 
+    ll ans = 0;
+    rep(i,bondNum.size()){
+        ans += bondNum[i]*(bondNum[i]-1)/2;
     }
-    ans1 -= m;
-    cout << ans1;
+    ans -= m;
+    cout << ans << endl;
 }
