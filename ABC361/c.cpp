eb652fe5fb89b9b7c11d6e07aa27ac8ab33bdc79
r04i02 <k10190202@gmail.com>
2024-07-06 23:08:47 +0900

    消した後を全探索

---
+++ ABC361/c.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
+bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
+//bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
+int main(){
+    int n,k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    sort(a.begin(),a.end());
+    ll ans = INF;
+    rep(i,k+1){
+        ll m = a[i];
+        ll ma = a[n-k+i-1];
+        ans = min(ma-m,ans);
+    }
+    cout << ans << endl;
+}
