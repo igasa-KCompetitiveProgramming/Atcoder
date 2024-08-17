846e601f3142dde83897771115525bedd528b351
r04i02 <k10190202@gmail.com>
2024-08-17 23:53:59 +0900

    全部左に避けて不足分を足し続ける

---
+++ ABC362/c.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
+bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
+//bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
+int main(){
+    int n;
+    cin >> n;
+    vector<ll> l(n),r(n);
+    rep(i,n){
+        cin >> l[i] >> r[i];
+    }
+    ll maxAns = 0;
+    ll minAns = 0;
+    rep(i,n){
+        maxAns += r[i];
+        minAns += l[i];
+    }
+    if(minAns < 0 && maxAns > 0){
+        cout << "Yes" << endl;
+    }else if(minAns == 0 || maxAns == 0){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+        return 0;
+    }
+    minAns = abs(minAns);
+    rep(i,n){
+        if(minAns > r[i]-l[i]){
+            cout << r[i] << " ";
+            minAns -= r[i] - l[i];
+        }else if(minAns == 0){
+            cout << l[i] << " ";
+        }else{
+            cout << l[i] + minAns << " ";
+            minAns = 0;
+        }
+    }
+}
