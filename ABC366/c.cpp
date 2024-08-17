eeba77cc73f60f27a3cf651690460c954387b1df
r04i02 <k10190202@gmail.com>
2024-08-18 00:58:08 +0900

    いわゆるmap最強回

---
+++ ABC366/c.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
+bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
+//bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
+int main(){
+    int n;
+    cin >> n;
+    vector<pint> q(n);
+    map<int,int> mp;
+    rep(i,n){
+        cin >> q[i].first;
+        if(q[i].first == 1){
+            cin >> q[i].second;
+            mp[q[i].second]++;
+        }else if(q[i].first == 2){
+            cin >> q[i].second;
+            mp[q[i].second]--;
+            if(mp[q[i].second] == 0){
+                mp.erase(q[i].second);
+            }
+        }else{
+            cout << mp.size() << endl;
+        }
+    }
+}
