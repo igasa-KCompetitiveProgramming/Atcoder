10753e48a8c215c5a2b3606c88d6c2277200037d
r04i02 <k10190202@gmail.com>
2024-08-17 23:51:27 +0900

    直角三角形かの判定

---
+++ ABC362/b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
+bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
+//bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
+int main(){
+    vector<int> x(3);
+    vector<int> y(3);
+    rep(i,3){
+        cin >> x[i] >> y[i];
+    }
+    rep(i,2){
+        if(x[i]  ==  x[i+1] && y[i] == y[i+1]){
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    if(x[2] == x[0] && y[2] == y[0]){
+        cout << "No" << endl;
+        return 0;
+    }
+    int a = pow(x[1]-x[0],2) + pow(y[1]-y[0],2);
+    int b = pow(x[2]-x[1],2) + pow(y[2]-y[1],2);
+    int c = pow(x[2]-x[0],2) + pow(y[2]-y[0],2);    
+    if(a+b == c || b+c == a || c+a == b){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+}
