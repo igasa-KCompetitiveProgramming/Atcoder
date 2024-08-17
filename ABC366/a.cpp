90b8942130f0fc3035ae64593595ee69aa95feb0
r04i02 <k10190202@gmail.com>
2024-08-18 00:54:08 +0900

    やる

---
+++ ABC366/a.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
+bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
+//bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
+int main(){
+    int n,t,a;
+    cin >> n >> t >> a;
+    bool flag = false;
+    if(t > a){
+        flag = true;
+    }
+    if(t == a){
+        cout << "No";
+        return 0;
+    }
+    int cnt = n - t - a;
+    if(cnt + t >= a && !flag){
+        cout << "No";
+        return 0;
+    }else if(cnt + a >= t && flag){
+        cout << "No";
+        return 0;
+    }
+    cout << "Yes";
+}
