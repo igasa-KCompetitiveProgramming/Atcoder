068f08db30572ee1cc257fee7c9c1e3a78768307
r04i02 <k10190202@gmail.com>
2024-09-07 23:28:53 +0900

    k番目以降を先に出力して最初に戻る

---
+++ ABC368/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+int main(){
+    int n,k;
+    cin >> n >> k;
+    vector<int> a(n);
+    rep(i,n) cin >> a[i];
+    rep(i,k){
+        cout << a[n-k+i] << " ";
+    }
+    rep(i,n-k){
+        cout << a[i] << " ";
+    }
+}
