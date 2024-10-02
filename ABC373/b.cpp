084fe059de53c1ed53583312766e0be4739f8841
r04i02 <k10190202@gmail.com>
2024-10-02 22:50:01 +0900

    最初に位置を記録 あとはやるだけ

---
+++ ABC373/b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+int main(){
+    string s;
+    cin >> s;
+    map<char, int> mp;
+    rep(i,s.size()){
+        mp[s[i]] = i;
+    }
+    int ans = 0;
+    rep(i,25){
+        ans += abs(mp['A'+i] - mp['A'+i+1]);
+    }
+    cout << ans;
+}
