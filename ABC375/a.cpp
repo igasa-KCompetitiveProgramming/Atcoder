f31d4acd1af6b5ee70845fc1be8df4d6444dcc48
r04i02 <k10190202@gmail.com>
2024-10-22 21:58:04 +0900

    #.#の個数を確認

---
+++ ABC375/a.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+int main(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int ans = 0;
+    rep(i,n-2){
+        if(s[i] == '#' && s[i+1] == '.' && s[i+2] == '#'){
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
