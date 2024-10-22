7fdd046121a07d729c87c2ff3eb9b91a72dc23d5
r04i02 <k10190202@gmail.com>
2024-10-22 21:55:23 +0900

    さん付けチェック

---
+++ ABC374/a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+int main(){
+    string s;
+    cin >> s;
+    int t = s.size();
+    if(s[t-3] == 's' && s[t-2] == 'a' && s[t-1] == 'n'){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+}
