73ccc05644db00b48bbb7c6aedc3e0f1c5e8fb19
r04i02 <k10190202@gmail.com>
2024-11-18 23:49:54 +0900

    feat:後ろからみるだけ

---
+++ ABC378/c.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    map<int,int> mp;
+    rep(i,n){
+        cin >> a[i];
+        if(mp[a[i]] == -2){
+        cout << 1 << " ";
+        mp[a[i]] = i;
+        continue;
+        }
+        if(!mp[a[i]]) cout << -1 << " ";
+        else cout << mp[a[i]]+1 << " ";
+        mp[a[i]] = i;
+        if(i == 0) mp[a[i]] = -2;
+    }
+}
