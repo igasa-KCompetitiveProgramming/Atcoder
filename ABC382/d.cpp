ab45fbe6bfc5a4664337545a128913bb4c0e984a
r04i02 <k10190202@gmail.com>
2024-11-30 22:59:55 +0900

    feat:再帰

---
+++ ABC382/d.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define v(a,i,b) vector<ll> a(i,b)
+#define vv(a,i,j,b) vector<vector<ll>> a(i, vector<ll>(j,b))
+#define vp(a,i,b,c) vector<pll>> a(i,{b,c})
+#define vvp(a,i,j,b,c) vector<vector<pll>> a(i, vector<>>(j,{b,c}))
+
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+//vectorのcin,cout
+template<typename T>
+std::istream &operator>>(std::istream&is,std::vector<T>&v){for(T &in:v){is>>in;}return is;}
+template<typename T>
+std::ostream &operator<<(std::ostream&os,const std::vector<T>&v){for(auto it=std::begin(v);it!=std::end(v);){os<<*it<<((++it)!=std::end(v)?" ":"");}return os;}
+//dequeのcin,cout
+template<typename T>
+std::istream &operator>>(std::istream&is,std::deque<T>&v){for(T &in:v){is>>in;}return is;}
+template<typename T>
+std::ostream &operator<<(std::ostream&os,const std::deque<T>&v){for(auto it=std::begin(v);it!=std::end(v);){os<<*it<<((++it)!=std::end(v)?" ":"");}return os;}
+//pairのcin,cout
+template<typename T,typename U>
+std::ostream &operator<<(std::ostream&os,const std::pair<T,U>&p){os<<p.first<<" "<<p.second;return os;}
+template<typename T,typename U>
+std::istream &operator>>(std::istream&is,std::pair<T,U>&p){is>>p.first>>p.second;return is;}
+//vector<vector<T>>のcout
+template<typename T>
+std::ostream &operator<<(std::ostream &os, const std::vector<std::vector<T>> &v) {
+for (const auto &row : v) {for (auto it = row.begin(); it != row.end(); ++it) { os << *it; if (std::next(it) != row.end()) { os << " "; } } os << "\n";} return os;}
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+vector<vector<ll>> a;
+
+void sai(vector<ll> &ans, ll f, ll g, ll cnt){
+    if(g > 0) ans[g] = (ans[g-1]-1)%10 + 1 + 10*g + f;
+    else ans[g] = f;
+    cnt -= f;
+    rep(ans.size()){
+        //cout << ans[i] << " ";
+    }
+    //cout << "f: " << f << " g: " << g << " cnt: " << cnt << endl;
+    if(cnt < 0){
+        return;
+    }else{
+        if(g == ans.size()-1){
+            a.pb(ans);
+            return;
+        }
+        for(ll i = 0; i < cnt + 1; i++){
+            sai(ans,i,g+1,cnt);
+        }
+    }
+}
+
+int main(){
+    LL(n,m);
+    for(ll i = 1; i < m - 10*(n-1) + 1; i++){
+        ll cnt = m - 10*(n-1);
+        v(ans,n,0);
+        sai(ans,i,0,cnt);
+    }
+    O(a.size());
+    rep(a.size()){
+        O(a[i]);
+    }
+}
