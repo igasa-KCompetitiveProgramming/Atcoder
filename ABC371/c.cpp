938022222d2d019f1043b9ed564dfc44c86dc69c
r04i02 <k10190202@gmail.com>
2024-12-03 17:00:36 +0900

    feat:順列全探索をしてminを調べる

--- ABC371/c.cpp
+++ ABC371/c.cpp
@@ -4,71 +4,108 @@ using ll = long long;
 using pint = pair<int,int>;
 using pll = pair<long long, long long>;
 
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define v(a,i,b) vector<ll> a(i,b)
+#define vv(a,i,j,b) vector<vector<ll>> a(i, vector<ll>(j,b))
+#define vp(a,i,b,c) vector<pll>> a(i,{b,c})
+#define vvp(a,i,j,b,c) vector<vector<pll>> a(i, vector<>>(j,{b,c}))
+
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+//vectorのcin,cout
+template<typename T>
+std::istream &operator>>(std::istream&is,std::vector<T>&v){for(T &in:v){is>>in;}return is;}
+template<typename T>
+std::ostream &operator<<(std::ostream&os,const std::vector<T>&v){for(auto it=std::begin(v);it!=std::end(v);){os<<*it<<((++it)!=std::end(v)?" ":"");}return os;}
+//dequeのcin,cout
+template<typename T>
+std::istream &operator>>(std::istream&is,std::deque<T>&v){for(T &in:v){is>>in;}return is;}
+template<typename T>
+std::ostream &operator<<(std::ostream&os,const std::deque<T>&v){for(auto it=std::begin(v);it!=std::end(v);){os<<*it<<((++it)!=std::end(v)?" ":"");}return os;}
+//pairのcin,cout
+template<typename T,typename U>
+std::ostream &operator<<(std::ostream&os,const std::pair<T,U>&p){os<<p.first<<" "<<p.second;return os;}
+template<typename T,typename U>
+std::istream &operator>>(std::istream&is,std::pair<T,U>&p){is>>p.first>>p.second;return is;}
+//vector<vector<T>>のcout
+template<typename T>
+std::ostream &operator<<(std::ostream &os, const std::vector<std::vector<T>> &v) {
+for (const auto &row : v) {for (auto it = row.begin(); it != row.end(); ++it) { os << *it; if (std::next(it) != row.end()) { os << " "; } } os << "\n";} return os;}
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
 int inf = 2147483647; // おおよそ2*10^9
 ll INF = 9223372036854775807; //おおよそ9*10^18
 //ull UINF == おおよそ1.8*10^19
 int main(){
-    int n,g,h;
-    cin >> n >> g;
-    vector<int> u(g),v(g);
-    vector<vector<bool>> G(n,vector<bool>(n));
-    rep(i,g){
-        cin >> u[i] >> v[i];
-        u[i]--;
-        v[i]--;
-        G[u[i]][v[i]] = true;
-        G[v[i]][u[i]] = true;
+    LL(n,mg);
+    vector<vector<bool>> g(n,vector<bool>(n,false)),h(n,vector<bool>(n,false));
+    rep(mg){
+        LL(tmp,res);
+        tmp--;res--;
+        g[tmp][res] = true;
+        g[res][tmp] = true;
     }
-    cin >> h;
-    vector<int> a(h),b(h);
-    vector<vector<bool>> H(n,vector<bool>(n));
-    rep(i,h){
-        cin >> a[i] >> b[i];
-        a[i]--;
-        b[i]--;
-        H[a[i]][b[i]] = true;
-        H[b[i]][a[i]] = true;
+    LL(mh);
+    rep(mh){
+        LL(tmp,res);
+        tmp--;res--;
+        h[tmp][res] = true;
+        h[res][tmp] = true;
     }
-    vector<vector<int>> cost(n,vector<int>(n,-1)); 
-    rep(i,n-1){
+    vector<vector<ll>> a(n-1);
+    rep(n-1){
         rep(j,n-i-1){
-            cin >> cost[i][j+i+1];
-            cost[j+i+1][i] = cost[i][j+i+1];
+            LL(tmp);
+            a[i].pb(tmp);
         }
     }
-    rep(i,n){
-        rep(j,n){
-            cout << G[i][j] << " ";
-        }
-        cout << endl;
-    }
-    rep(i,n){
-        rep(j,n){
-            cout << H[i][j] << " ";
-        }
-        cout << endl;
-    }
-    vector<int> tmp(n);
+
+    v(tmp,n,0);
     rep(i,n){
         tmp[i] = i;
     }
-    int ans = inf;
+    ll ans = INF;
     while(true){
-        int kari = 0;
-        int cnt = 0;
-        rep(i,n){
-            rep(j,n){
-                if(G[i][j] != H[tmp[i]][tmp[j]]){
-                    kari += cost[i][j];
-                    cnt++;
+        ll cnt = 0;
+        rep(i,n-1){
+            rep(j,n-i-1){
+                if(h[i][i+j+1] != g[tmp[i]][tmp[i+j+1]]){
+                    cnt += a[i][j];
                 }
             }
         }
-        cout << cnt << " ";
-        ans = min(ans,kari);
+        ans = min(ans,cnt);
         if(!next_permutation(tmp.begin(),tmp.end())) break;
     }
-    cout << ans;
+    O(ans);
 }
