351d1ce8cce59cb8c3bf69dd1a2ce929cfbe9fde
r04i02 <k10190202@gmail.com>
2025-01-27 23:01:25 +0900

    feat : 辞書順sortしたら見ている黒色のものよりも左側かどうかの判定が不要になることを利用する

--- ABC386/d.cpp
+++ ABC386/d.cpp
@@ -4,11 +4,6 @@ using ll = long long;
 using pint = pair<int,int>;
 using pll = pair<long long, long long>;
 
-#define v(a,i,b) vector<ll> a(i,b)
-#define vv(a,i,j,b) vector<vector<ll>> a(i, vector<ll>(j,b))
-#define vp(a,i,b,c) vector<pll>> a(i,{b,c})
-#define vvp(a,i,j,b,c) vector<vector<pll>> a(i, vector<>>(j,{b,c}))
-
 #define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
 #define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
 #define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
@@ -67,107 +62,24 @@ int inf = 2147483647; // おおよそ2*10^9
 ll INF = 9223372036854775807; //おおよそ9*10^18
 //ull UINF == おおよそ1.8*10^19
 int main(){
-    ll n,m;
-    cin >> n >> m;
-    vector<ll> x(m),y(m);
-    vector<char> c(m);
-    rep(m) cin >> y[i] >> x[i] >> c[i];
-    map<ll,ll> bMap,wMap;
+    LL(n,m);
+    vector<tuple<ll,ll,char>> p(m);
     rep(m){
-        if(c[i] == 'B'){
-            bMap[x[i]] = -1;
-            //wMap[x[i]] = inf;
-        }else{
-            wMap[x[i]] = inf;
-            //bMap[x[i]] = -1;
+        LL(a,b);
+        char c;
+        cin >> c;
+        p[i] = make_tuple(a,b,c);
+    }
+    sort(p.begin(), p.end());
+    ll minH = inf;
+    rep(m){
+        if(get<2>(p[i]) == 'W') minH = min(minH, get<1>(p[i]));
+        else{
+            if(get<1>(p[i]) >= minH){
+                cout << "No" << endl;
+                return 0;
+            }
         }
     }
-    rep(m){
-        if(c[i] == 'B'){
-            bMap[x[i]] = max(bMap[x[i]],y[i]);
-        }else{
-            wMap[x[i]] = min(wMap[x[i]],y[i]);
-        }
-    }
-
-    vector<ll> b,w;
-    for(auto i : bMap){
-        b.pb(i.fi);
-    }
-    for(auto i : wMap){
-        w.pb(i.fi);
-    }
-    bool ans = true;
-    if(b.size() == 0 || w.size() == 0){
-        cout << "Yes" << endl;
-        return 0;
-    }
-
-    /*
-
-    for(auto i : b){
-        cout << i << spa;
-    }
-    cout << endl;
-    for(auto i : w){
-        cout << i << spa;
-    }
-    cout << endl;
-
-    */
-    rep(m){
-        if(c[i] == 'B'){
-            int left = -1;
-            int right = w.size()-1; // rightは各自設定
-            while(right - left > 1){ //rightは配列[mid]より小さい最大値に、leftは配列[mid]より大きい最小値になる
-                int mid = left + (right - left) / 2;
-                if(w[mid] >= x[i]){ //配列[mid]より比較対象が小さい場合
-                    right = mid;
-                }else{
-                    left = mid;
-                }
-                if(right < left) break;
-            }
-            //cout << left << spa << right << endl;
-            //if(left != -1) cout << wMap[left] << endl;
-            if(left == -1) continue;
-            ll minH = inf;
-            if(left != -1) minH = min(minH,wMap[left]);
-            if(right != w.size()) minH = min(minH,wMap[right]);
-            if(wMap[left] <= y[i]){
-                ans = false;
-            }
-            if(minH >= y[i]){
-                ans = false;
-            }
-        }else{
-            //cout << x[i] <<spa;
-            int left = -1;
-            int right = b.size()-1; // rightは各自設定
-            while(right - left > 1){ //rightは配列[mid]より小さい最大値に、leftは配列[mid]より大きい最小値になる
-                int mid = left + (right - left) / 2;
-                if(b[mid] >= x[i]){ //配列[mid]より比較対象が小さい場合
-                    right = mid;
-                }else{
-                    left = mid;
-                }
-                if(right < left) break;
-            }
-            //cout << left << spa << right << endl;
-            ll maxH = -1;
-            if(left != -1) maxH = max(maxH,bMap[left]);
-            if(right != b.size()) maxH = max(maxH,bMap[right]);
-            if(right == (ll)b.size()) continue;
-            //cout << maxH << spa << bMap[right] << spa << y[i] << endl;
-            if(bMap[right] <= y[i]){
-                ans = false;
-            }
-            if(maxH <= y[i]){
-                ans = false;
-            }
-        }
-        //if(!ans) cout << i << endl;
-    }
-    if(ans) cout << "Yes" << endl;
-    else cout << "No" << endl;
+    cout << "Yes" << endl;
 }
