28fc056163903ac76d750ad97fc37210b785421d
r04i02 <k10190202@gmail.com>
2025-04-05 10:32:56 +0900

    feat : score=	26229272

--- AHC045/main.cpp
+++ AHC045/main.cpp
@@ -42,31 +42,65 @@ int inf = 2147483647; // おおよそ2*10^9
 ll INF = 9223372036854775807; //おおよそ9*10^18
 //ull UINF == おおよそ1.8*10^19
 
-class UnionFind{
-    public:
-    UnionFind() = default;
+#include <bits/stdc++.h>
+using namespace std;
 
-    explicit UnionFind(size_t n) : m_parents(n){
-        iota(m_parents.begin(), m_parents.end(), 0);
+struct UnionFind {
+    vector<int> parent, rank, fixedRoot;
+    
+    UnionFind(int n) : parent(n), rank(n, 0), fixedRoot(n, -1) {
+        iota(parent.begin(), parent.end(), 0);  // 各要素は最初自分自身が根
     }
 
-    ll root(ll i){
-        if(m_parents[i] == i) return i;
-        return(m_parents[i] = root(m_parents[i]));
+    int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);  // 経路圧縮
     }
 
-    void merge(ll a,ll b){
-        a = root(a);
-        b = root(b);
-        if(a != b) m_parents[b] = a;
+    void initialMerge(int u, int v, int x) {
+        int rootU = find(u);
+        int rootV = find(v);
+        int rootX = find(x);
+
+        if (rootU == rootV) return; // 既に同じグループなら何もしない
+
+        // `x` を強制的に新しい根とする
+        parent[rootU] = rootX;
+        parent[rootV] = rootX;
+        fixedRoot[rootX] = x;  // `x` を固定根に設定
     }
 
-    bool connecter(ll a,ll b){
-        return root(a) == root(b);
+    void merge(int u, int v) {
+        int rootU = find(u);
+        int rootV = find(v);
+
+        if (rootU == rootV) return; // すでに同じグループなら何もしない
+
+        // 固定された根があるかチェック
+        if (fixedRoot[rootU] != -1 && fixedRoot[rootV] == -1) {
+            parent[rootV] = rootU;  // `rootU` を根として維持
+        } else if (fixedRoot[rootV] != -1 && fixedRoot[rootU] == -1) {
+            parent[rootU] = rootV;  // `rootV` を根として維持
+        } else if (fixedRoot[rootU] != -1 && fixedRoot[rootV] != -1) {
+            // 両方固定済みなら何もしない（またはエラー）
+        } else {
+            // どちらも未固定なら通常の Union-Find のルール
+            if (rank[rootU] < rank[rootV]) swap(rootU, rootV);
+            parent[rootV] = rootU;
+            if (rank[rootU] == rank[rootV]) rank[rootU]++;
+        }
     }
-    private:
-    vector<ll> m_parents;
+
+    int getRoot(int x) {
+        int root = find(x);
+        return fixedRoot[root] != -1 ? fixedRoot[root] : root;
+    }
+
+    bool connecter(int u, int v) {
+        return find(u) == find(v);
+    }
 };
+
 
 class solve{
   public:
@@ -88,6 +122,8 @@ class solve{
     vector<vector<pll>> group_vertex;
 
     vector<pll> test_edge;
+    vector<bool> used;
+
 
     void input(){
         cin >> city_num >> group_num >> query_num >> query_max_size >> city_max_size;
@@ -99,6 +135,7 @@ class solve{
         left.resize(city_num);
         right.resize(city_num);
         test_edge.resize(city_num);
+        used.resize(city_num, false);
 
         city_center_point.resize(city_num);
         city_distance.resize(city_num);
@@ -109,7 +146,7 @@ class solve{
         rep(city_num) city_center_point[i] = {(left[i].fi + right[i].fi)/2, (left[i].se + right[i].se)/2, (right[i].fi - left[i].fi)*(right[i].se - right[i].se)};
 
         rep(i,city_num){
-            rep(j,city_num){
+            rep(j,i+1,city_num){
                 if(i == j) continue;
                 ll distance = pow((get<0>(city_center_point[i]) - get<0>(city_center_point[j])),2) + pow(get<1>(city_center_point[i]) - get<1>(city_center_point[j]), 2);
                 city_distance[i].pb({distance, j});
@@ -151,8 +188,86 @@ class solve{
         }
     }
 
+    void make_first_answer_(){
+        UnionFind uf(city_num);
+        priority_queue<tuple<ll,ll,ll>,vector<tuple<ll,ll,ll>>,greater<tuple<ll,ll,ll>>> pq;
+        priority_queue<tuple<ll,ll,ll>,vector<tuple<ll,ll,ll>>,greater<tuple<ll,ll,ll>>> pq2;
+        rep(city_num){
+            rep(j,i+1,city_num){
+                ll distance = pow((get<0>(city_center_point[i]) - get<0>(city_center_point[j])),2) + pow(get<1>(city_center_point[i]) - get<1>(city_center_point[j]), 2);
+                pq.push({distance, i, j});
+            }
+        }
+
+        pq2 = pq;
+
+        map<ll, ll> group_idx;
+        vector<vector<ll>> group_list(group_num);
+
+        int cnt = city_num;
+
+        rep(group_num){
+            if(group_size[i] == 1){
+                cnt--;
+                continue;
+            }
+            while(!pq.empty()){
+                auto [dist, u, v] = pq.top();
+                pq.pop();
+                if(used[u] || used[v]){
+                    if(used[v]) swap(u, v);
+                    if(group_merge(uf.getRoot(u), u, v, group_idx, group_list)){
+                        cnt--;
+                        uf.merge(u, v);
+                    }
+                    continue;
+                }
+                uf.initialMerge(u, v, u);
+                group_idx[u] = i;
+                group_list[i].pb(u);
+                group_list[i].pb(v);
+                used[u] = true;
+                used[v] = true;
+                cnt -= 2;
+                break;
+            }
+        }
+
+        pq = pq2;
+
+        while(!pq.empty()){
+            auto [dist, u, v] = pq.top();
+            pq.pop();
+            if(used[v]) swap(u, v);
+            if(group_merge(uf.getRoot(u), u, v, group_idx, group_list)){
+                cnt--;
+                uf.merge(u, v);
+            }
+            if(cnt == 0) break;
+        }
+
+
+        rep(i, group_num) {
+            if(group_size[i] == 1) continue;
+            rep(j, group_list[i].size()) {
+                group[i].pb(group_list[i][j]);
+            }
+        }
+    }
+
+    bool group_merge(int r, int u, int v, map<ll,ll>& group_idx, vector<vector<ll>>& group_list){
+        if(used[u] == used[v]) return false;
+        int idx = group_idx[r];
+        if(group_list[idx].size() == group_size[idx]) return false;
+        group_list[idx].pb(v);
+        used[u] = true;
+        used[v] = true;
+        return true;
+    }
+
     void make_second_answer(){
         rep(group.size()){
+            if(group[i].size() == 1) continue;
             int tmp = 0;
             while(true){
                 if(tmp + 1 >= group[i].size()) break;
@@ -177,6 +292,7 @@ class solve{
 
     void make_second_answer_test(){
         rep(group.size()){
+            if(group[i].size() == 1) continue;
             int tmp = 0;
             while(true){
                 if(tmp + 1 >= group[i].size()) break;
@@ -218,14 +334,36 @@ class solve{
     }
 
     void answer(){
+        vector<bool> use(city_num, false);
         cout << '!' << endl;
         rep(i,group_num){
-            rep(j,group[i].size()){
-                cout << group[i][j] << spa;
+            //if(group_size[i] != 1 && (group[i].size() != group_size[i])) cout << "error" << spa;
+            //cout << spa << group_size[i] << spa << group[i].size() << endl;
+            if(group_size[i] == 1){
+                rep(j,city_num){
+                    if(used[j]) continue;
+                    else{
+                        cout << j << endl;
+                        used[j] = true;
+                        use[j] = true;
+                        break;
+                    }
+                }
+            }else{
+                rep(j,group[i].size()){
+                    cout << group[i][j] << spa;
+                    use[group[i][j]] = true;
+                }
+                cout << endl;
+                rep(j,group_vertex[i].size()){
+                    cout << group_vertex[i][j].fi << spa << group_vertex[i][j].se << endl;
+                }
             }
-            cout << endl;
-            rep(j,group_vertex[i].size()){
-                cout << group_vertex[i][j].fi << spa << group_vertex[i][j].se << endl;
+        }
+        rep(i,city_num){
+            if(!use[i]){
+                cout << "error" << spa;
+                cout << i << endl;
             }
         }
     }
@@ -234,9 +372,10 @@ class solve{
 int main(){
     solve s;
     s.input();
-    s.test_input();
-    s.make_first_answer();
-    //s.make_second_answer();
-    s.make_second_answer_test();
+    //s.test_input();
+    //s.make_first_answer();
+    s.make_first_answer_();
+    s.make_second_answer();
+    //s.make_second_answer_test();
     s.answer();
 }
