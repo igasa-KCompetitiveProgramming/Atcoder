34e526e2aa0e4aad9596ec6fcac5707fdf535803
r04i02 <k10190202@gmail.com>
2025-04-24 10:57:29 +0900

    feat : ahc032

---
+++ AHC032/main.cpp
@@ -0,0 +1,146 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+
+const int mod = 998244353;
+class mint {
+    long long x;
+public:
+    mint(long long x=0) : x((x%mod+mod)%mod) {}
+    mint operator-() const { 
+      return mint(-x);
+    }
+    mint& operator+=(const mint& a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint& a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const  mint& a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+    mint operator+(const mint& a) const {
+        mint res(*this);
+        return res+=a;
+    }
+    mint operator-(const mint& a) const {
+        mint res(*this);
+        return res-=a;
+    }
+    mint operator*(const mint& a) const {
+        mint res(*this);
+        return res*=a;
+    }
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+    // for prime mod
+    mint inv() const {
+        return pow(mod-2);
+    }
+    mint& operator/=(const mint& a) {
+        return (*this) *= a.inv();
+    }
+    mint operator/(const mint& a) const {
+        mint res(*this);
+        return res/=a;
+    }
+
+    friend ostream& operator<<(ostream& os, const mint& m){
+        os << m.x;
+        return os;
+    }
+};
+
+
+class solve{
+  public:
+
+    
+
+    ll n,m,max_query;
+    vector<vector<ll>> a;
+    vector<vector<vector<ll>>> s;
+
+    vector<tuple<ll,ll,ll>> ans;
+
+    void input(){
+        cin >> n >> m >> max_query;
+
+        a.resize(n, vector<ll>(m));
+        s.resize(n+1, vector<vector<ll>>(n+1, vector<ll>(n+1)));
+
+        rep(i,n) rep(j,m) cin >> a[i][j];
+        rep(n) rep(j,n) rep(k,n) cin >> s[i][j][k];
+    }
+
+    void make_first_answer(){
+        rep(max_query){
+            int stamp_idx = rand() % m;
+            int x = rand() % (n-2);
+            int y = rand() % (n-2);
+            ans.push_back({stamp_idx, x, y});
+        }
+    }
+
+    void answer(){
+        cout << ans.size() << endl;
+        for(auto [stamp_idx, x, y] : ans){
+            cout << stamp_idx << " " << x << " " << y << endl;
+        }
+    }
+};
+
+int main(){
+    solve s;
+    s.input();
+    s.make_first_answer();
+    s.answer();
+}
