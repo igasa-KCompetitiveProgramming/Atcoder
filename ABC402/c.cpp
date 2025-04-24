e1ed1efb5578a52157bf9db0caf2c981e02b449c
r04i02 <k10190202@gmail.com>
2025-04-24 10:56:17 +0900

    feat : 食品ごとに食べられるようになる日を求めた後、累積和

---
+++ ABC402/c.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+int main(){
+    LL(n,m);
+    vector<ll> k(m),b(n);
+    map<ll,ll> mp;
+    vector<vector<ll>> a(m);
+    rep(m){
+        cin >> k[i];
+        a[i].resize(k[i]);
+        rep(j,k[i]){
+            cin >> a[i][j];
+            a[i][j]--;
+        }
+    }
+    rep(n) cin >> b[i];
+    rep(n) b[i]--;
+    rep(n) mp[b[i]] = i+1;
+    vector<ll> ans(n,0);
+    rep(m){
+        ll cnt = 0;
+        rep(j,k[i]){
+            cnt = max(cnt,mp[a[i][j]]);
+        }
+        ans[cnt-1]++;
+    }
+    ll cnt = 0;
+    rep(n){
+        ll tmp = ans[i];
+        ans[i] += cnt;
+        cnt += tmp;
+    }
+    rep(n){
+        cout << ans[i] << endl;
+    }
+}
