e67e8237820c62ba592298b88c2de80073e7cf10
r04i02 <k10190202@gmail.com>
2025-05-10 23:48:05 +0900

    feat : 単純サイクルグラフかどうか判定

---
+++ ABC404/c.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+int main(){
+    LL(n,m);
+    if(n != m){
+        cout << "No" << endl;
+        return 0;
+    }
+    vector<ll> a(n),b(m);
+    rep(n) cin >> a[i] >> b[i];
+    vector<vector<ll>> g(n);
+    rep(m){
+        g[a[i]-1].push_back(b[i]-1);
+        g[b[i]-1].push_back(a[i]-1);
+    }
+    vector<bool> used(n, false);
+    ll now = 0; ll start = 0;
+    while(true){
+        //cout << spa << now << spa << used[3] << endl;
+        if(used[now]){
+            if(now == start) break;
+            else{
+                cout << "No" << endl;
+                return 0;
+            }
+        }
+        used[now] = true;
+        for(auto x : g[now]){
+            if(!used[x]){
+                now = x;
+                break;
+            }
+            if(x == start){
+                now = x;
+            }
+        }
+    }
+    rep(n){
+        if(!used[i]){
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+}
