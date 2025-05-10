3c45db22e3c71ce8de5256347d5721028fdd7474
r04i02 <k10190202@gmail.com>
2025-05-10 23:49:43 +0900

    feat : a_iが1になったら必ずbreakするdp

---
+++ ABC404/e.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
+int inf = 2147483647; // おおよそ2*10^9
+ll INF = 9223372036854775807; //おおよそ9*10^18
+//ull UINF == おおよそ1.8*10^19
+int main(){
+    LL(n);
+    n--;
+    vector<ll> c(n),a(n);
+    rep(n) cin >> c[i];
+    rep(n) cin >> a[i];
+    reverse(c.begin(), c.end());
+    reverse(a.begin(), a.end());
+
+    ll ans = 0;
+    bool flag = false;
+    vector<ll> dp(n + 1, INF);
+    rep(n){
+        dp[i] = 0;
+        if(a[i] == 1) break;
+    }
+    rep(n){
+        if(a[i] == 1) flag = true;
+        if(!flag) continue;
+        for(int j = i + 1; j <= i + c[i]; j++){
+            if(j > n){
+                break;
+            }
+            dp[j] = min(dp[j], dp[i] + 1);
+            if(a[j] == 1){
+                break;
+            }
+        }
+    }
+    cout << dp[n] << endl;
+}
