22a600895ba2da78d08f290cca86b2491f5dd3a3
r04i02 <k10190202@gmail.com>
2025-05-26 20:45:53 +0900

    feat : 等比数列の和と繰り返し二乗法

--- ABC357/d.cpp
+++ ABC357/d.cpp
@@ -1,62 +1,89 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using ull = unsigned long long;
 using pint = pair<int,int>;
 using pll = pair<long long, long long>;
 
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
-#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
-#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
-int inf = 2147483647; // おおよそ2*10^9
-ll INF = 9223372036854775807; //おおよそ9*10^18
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
+//int INF = 2147483647; // おおよそ2*10^9
+ll inf = 1LL<<60; //おおよそ10^18
 //ull UINF == おおよそ1.8*10^19
-bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
-bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
-//bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
-ll MOD = 998244353;
 
-ll POW(ll x){
-    ll ans = 1;
-    rep(i,x){
-        ans *= 10;
-        ans %= MOD;
-    }
-    return ans;
-}
+const int mod = 998244353;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = mint::pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
 
-ll POWER(ll n){
-    ll m = n;
-    string s = to_string(n);
-    ll ans = 0;
-    ll amari = POW(s.size());
-    while(n != 0){
-        if(n%2 == 0){
-            amari *= POW(s.size());
-            ans *= POW(s.size());
-            ans %= MOD;
-            ans += amari/(9);
-            amari %= (9);
-            ans %= MOD;
-        }
-        amari *= POW(s.size());
-        ans *= POW(s.size());
-        ans %= MOD;
-        ans += amari/(9);
-        amari %= (9);
-        ans %= MOD;
-        n /= 2;
-        cout << ans << " " << amari << endl;
-    }
-    ans *= m;
-    ans %= MOD;
-    ans--;
-    return (ans+amari)%MOD;
-}
+  // for prime mod
+  mint inv() const { return mint::pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
 
 int main(){
-    ll n;
-    cin >> n;
-    cout << POWER(n) << endl;
+    ll n,x;
+	mint r = 1;
+	cin >> n;
+	x = n;
+	while(x){
+		x /= 10;
+		r *= mint(10);
+	}
+	mint ans = mint(n) * (r.pow(n)-mint(1)) * ((r-mint(1)).inv());
+    cout << ans << endl;
 }
