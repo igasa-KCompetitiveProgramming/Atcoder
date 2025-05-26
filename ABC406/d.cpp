d423c26931e3d829c0e1b04ee3d67b7d005469b3
r04i02 <k10190202@gmail.com>
2025-05-26 20:52:27 +0900

    feat : 何かで管理して消す回数は高々nであることに注目、vector<set<ll>>を使う

---
+++ ABC406/d.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
+//int INF = 2147483647; // おおよそ2*10^9
+ll inf = 1LL<<60; //おおよそ10^18
+//ull UINF == おおよそ1.8*10^19
+int main(){
+    LL(h,w,n);
+    vector<ll> x(n),y(n);
+    rep(n) cin >> x[i] >> y[i];
+    rep(n){
+        x[i]--;
+        y[i]--;
+    }
+    vector<set<ll>> sx(h+1),sy(w+1);
+    rep(n){
+        sx[x[i]].insert(y[i]);
+        sy[y[i]].insert(x[i]);
+    }
+    LL(q);
+    rep(q){
+        LL(a);
+        if(a == 1){
+            LL(b);
+            b--;
+            cout << sx[b].size() << endl;
+            for(auto x : sx[b]){
+                sy[x].erase(b);
+            }
+            sx[b].clear();
+        }else if(a == 2){
+            LL(b);
+            b--;
+            cout << sy[b].size() << endl;
+            for(auto y : sy[b]){
+                sx[y].erase(b);
+            }
+            sy[b].clear();
+        }
+    }
+}
