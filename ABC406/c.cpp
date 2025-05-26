fe78f41cda3c3a557720a8f2aea8e65c30c58d1c
r04i02 <k10190202@gmail.com>
2025-05-26 20:50:58 +0900

    feat : ラングレンス圧縮し、<の前後の連続する>の数の積が答え

---
+++ ABC406/c.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
+//int INF = 2147483647; // おおよそ2*10^9
+ll inf = 1LL<<60; //おおよそ10^18
+//ull UINF == おおよそ1.8*10^19
+
+int main(){
+    LL(n);
+    vector<ll> a(n);
+    rep(n) cin >> a[i];
+    vector<pair<char, ll>> sz;
+    rep(n - 1){
+        if(a[i] < a[i + 1]){
+            if(sz.empty() || sz.back().fi == '>'){
+                sz.pb({'<', 1});
+            }else sz.back().se++;
+        }else{
+            if(sz.empty() || sz.back().fi == '<'){
+                sz.pb({'>', 1});
+            }else sz.back().se++;
+        }
+    }
+    ll ans = 0;
+    rep(sz.size() - 2){
+        if(sz[i].fi == '<') ans += sz[i].se * sz[i + 2].se;
+    }
+    cout << ans << endl;
+}
