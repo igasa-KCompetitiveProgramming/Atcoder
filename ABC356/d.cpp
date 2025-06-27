20f8fc976738950c33b82e979f7141043b29582e
r04i02 <k10190202@gmail.com>
2025-06-27 14:21:11 +0900

    feat : 各桁に関して考える。商と余りを使う

--- ABC356/d.cpp
+++ ABC356/d.cpp
@@ -1,60 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using ull = unsigned long long;
 using pint = pair<int,int>;
 using pll = pair<long long, long long>;
 
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
-#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
-#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
-int inf = 2147483647; // おおよそ2*10^9
-ll INF = 9223372036854775807; //おおよそ9*10^18
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
+//int INF = 2147483647; // おおよそ2*10^9
+ll inf = 1LL<<60; //おおよそ10^18
 //ull UINF == おおよそ1.8*10^19
-bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
-bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
-//bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
+
+const ll mod = 998244353;
+
 int main(){
-    ll mod = 998244353;
-    ull n,m;
-    cin >> n >> m;
-    ull cnt = 0;
-    ull tmp2 = n;
-    while(true){
-        if(tmp2 == 0) break;
-        tmp2 /= 2;
-        cnt++;
-    }
-    bitset<60> a(m);
-    vector<ull> pow2;
-    ull tmp = 2;
-    rep(i,60){
-        tmp*=2;
-        pow2.push_back(tmp);
-    }
-    ull ans = 0;
+    LL(n,m);
+    ll ans = 0;
     n++;
-    rep(i,cnt){
-        if(a[i] == 0){
-            continue;
-        }
-        if(i == 0){
-            n--;
-            ans += (((n-1)/2)+1)%mod;
-            n++;
+    rep(61){
+        if((1LL<<i) & m){
+            //cout << (1LL<<i) << endl;
+            ll r = n / (2*(1LL<<i)), q = n % (2*(1LL<<i)), c = (1LL<<i);
+            //cout << r << spa << q << spa << r * c << endl;
+            ans += r * c;
+            q -= c;
+            ans += max(0LL, q);
             ans %= mod;
-            continue;
         }
-        cout << "i=" << i << endl;
-        ll q = n/pow2[i-1];
-        ans += q*(pow2[i-1]/2)%mod;
-        ans %= mod;
-        q = n%pow2[i-1];
-        if(q<0) continue;
-        ans += q%pow2[i-1]/2;
-        ans %= mod;
-        cout << ans << endl;
     }
-    cout << ans%mod;
+    cout << ans << endl;
 }
