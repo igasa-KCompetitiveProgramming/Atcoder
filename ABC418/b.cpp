a7899a1c3aeee372c23128f713c53fdb129464d6
r04i02 <k10190202@gmail.com>
2025-08-11 17:25:13 +0900

    feat : 全部分文字列を列挙して探索

---
+++ ABC418/b.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
+//int INF = 2147483647; // おおよそ2*10^9
+ll inf = 1LL<<60; //おおよそ10^18
+//ull UINF == おおよそ1.8*10^19
+int main(){
+    string s;
+    cin >> s;
+    long double ans = 0;
+    rep(i, s.size()){
+        rep(j, i + 3, s.size() + 1){
+            if(i == j) continue;
+            string sub = s.substr(i, j - i);
+            if(sub[0] == 't' && sub[sub.size() - 1] == 't'){
+                long double score = 0;
+                rep(k, sub.size()){
+                    if(sub[k] == 't') score += 1.0;
+                }
+                ans = max(ans, (score - 2) / (sub.size() - 2));
+                cout << i << spa << j << endl;
+                cout << (score - 2) / (sub.size() - 2) << endl;
+                cout << sub.size() - 2 << endl;
+            }else continue;
+        }
+    }
+    cout << fixed << setprecision(10) << ans << endl;
+}
