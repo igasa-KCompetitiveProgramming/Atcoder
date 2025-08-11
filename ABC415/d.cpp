6ac31ec2d250c8cb686a9e494fb0cf97e8cb771f
r04i02 <k10190202@gmail.com>
2025-08-11 17:17:35 +0900

    feat : 空き瓶ともらえるコーラの差が小さいものから貪欲

---
+++ ABC415/d.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
+//int INF = 2147483647; // おおよそ2*10^9
+ll inf = 1LL<<60; //おおよそ10^18
+//ull UINF == おおよそ1.8*10^19
+int main(){
+    LL(n,m);
+    vector<ll> a(m),b(m);
+    rep(m) cin >> a[i] >> b[i];
+    vector<pair<ll, ll>> temp;
+    rep(m){
+        temp.pb({a[i] - b[i], i});
+    }
+    sort(temp.begin(), temp.end());
+    ll ans = 0;
+    rep(m){
+        ll x,y;
+        x = a[temp[i].se]; y = b[temp[i].se];
+        ll diff = x - y;
+        ll res = n - x;
+        if(res < 0) continue;
+        ll cnt = res / diff;
+        cnt++;
+        ans += cnt;
+        n -= cnt * diff;
+        //cout << n << spa << ans << endl;
+    }
+    cout << ans << endl;
+}
