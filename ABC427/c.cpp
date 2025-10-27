59afad6540a7400a5f4a8afcb340e2ad4847dd58
r04i02 <k10190202@gmail.com>
2025-10-27 15:18:20 +0900

    二部グラフにするためにbit全探索して最小値を求める

---
+++ ABC427/c.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
+//int INF = 2147483647; // おおよそ2*10^9
+ll inf = 1LL<<60; //おおよそ10^18
+//ull UINF == おおよそ1.8*10^19
+
+ll check(bitset<10> &a, vector<vector<bool>> &g){
+    ll ans = 0;
+    rep(g.size()){
+        rep(j, g.size()){
+            if(!g[i][j]) continue;
+            if(a[i] != a[j]) continue;
+            ans++;
+        }
+    }
+    ans /= 2;
+    return ans;
+}
+
+int main(){
+    LL(n,m);
+    vector<int> u(m),v(m);
+    vector<vector<bool>> g(n, vector<bool>(n, false));
+    rep(m){
+        cin >> u[i] >> v[i];
+        u[i]--; v[i]--;
+        g[u[i]][v[i]] = true;
+        g[v[i]][u[i]] = true;
+    }    
+    queue<vector<bool>> q;
+    q.push({});
+    ll ans = inf;
+    rep(pow(2,n)){
+        bitset<10> a = i;
+        ans = min(ans,check(a, g));
+    }
+    cout << ans << endl;
+}
