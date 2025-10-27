e3a818d7f73aea3cbb6ca5878d2f97f80f7d3a3f
r04i02 <k10190202@gmail.com>
2025-10-27 15:10:26 +0900

    累積和、最初の位置がずれるので添え字ガチャする

---
+++ ABC425/c.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
+//int INF = 2147483647; // おおよそ2*10^9
+ll inf = 1LL<<60; //おおよそ10^18
+//ull UINF == おおよそ1.8*10^19
+int main(){
+    LL(n,q);
+    vector<ll> a(n);
+    rep(n) cin >> a[i];
+    vector<ll> ruiseki(n + 1, 0);
+    rep(n) ruiseki[i + 1] = ruiseki[i] + a[i];
+    ll top = 0;
+    rep(query, q){
+        LL(tmp);
+        if(tmp == 1){
+            LL(c);
+            top += c;
+            top %= n;
+        }else{
+            LL(l,r);
+            l--;
+            ll ans = 0;
+            if(top + r >= n && top + l < n){
+                ans += ruiseki[n] - ruiseki[l + top];
+                ans += ruiseki[(top + r) % n];
+                //cout << l << spa << r << spa << top << endl;
+                //cout << ruiseki[n] << spa << ruiseki[l + top] << endl;
+                //cout << ruiseki[(top + r) % n] << endl;
+            }else{
+                //cout << (r + top) % n << spa << (l + top) % n << endl;
+                ans = ruiseki[(r + top) % n] - ruiseki[(l + top) % n];
+            }
+            cout << ans << endl;
+        }
+        //cout << top << endl;
+    }
+}
