665d0292e95b704b2350330fbc0a2905703e1847
r04i02 <k10190202@gmail.com>
2025-10-27 14:58:16 +0900

    2分木を作るのが最小、アンバランスさの最大は1

---
+++ ABC422/d.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rrep1(a)          for(ll i = (ll)(a-1); i >= (ll)0 ; i--)
+#define rrep2(i, a)       for(ll i = (ll)(a-1); i >= (ll)0; i--)
+#define rrep3(i, a, b)    for(ll i = (ll)(a-1); i >=(b); i--)
+#define rrep4(i, a, b, c) for(ll i = (ll)(a-1); i >=(b); i -= (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+
+#define rep1(a)          for(ll i = 0; i < (ll)(a); i++)
+#define rep2(i, a)       for(ll i = 0; i < (ll)(a); i++)
+#define rep3(i, a, b)    for(ll i = (a); i < (ll)(b); i++)
+#define rep4(i, a, b, c) for(ll i = (a); i < (ll)(b); i += (c))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define fi first
+#define se second
+#define pb push_back
+#define spa " "
+
+//！？！？
+#define O print
+
+inline void scan(){}
+template<class Head,class... Tail>
+inline void scan(Head&head,Tail&... tail){std::cin>>head;scan(tail...);}
+#define LL(...) ll __VA_ARGS__;scan(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;scan(__VA_ARGS__)
+
+#pragma GCC diagnostic ignored "-Wunused-value"
+void print(){cout << '\n';}
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){cout << a;(std::cout << ... << (cout << ' ', b));cout << '\n';}
+#pragma GCC diagnostic warning "-Wunused-value"
+
+//int INF = 2147483647; // おおよそ2*10^9
+ll inf = 1LL<<60; //おおよそ10^18
+//ull UINF == おおよそ1.8*10^19
+int main(){
+    LL(n,k);
+    vector<ll> bef, aft;
+    bef.pb(k);
+    rep(n){
+        rep(j, bef.size()){
+            ll now = bef[j];
+            ll r = now % 2;
+            ll x = now / 2;
+            aft.pb(x);
+            if(r) aft.pb(x + 1);
+            else aft.pb(x);
+        }
+        bef = aft;
+        aft.clear();
+    }
+    ll ans = 0;
+    rep(bef.size()){
+        //cout << i << spa << i + 1 << endl;
+        //cout << bef[i] << spa << bef[i + 1] << endl;
+        ans = max(ans, abs(bef[i] - bef[i + 1]));
+        i++;
+    }
+    cout << ans << endl;
+    rep(bef.size()){
+        cout << bef[i] << spa;
+    }
+}
